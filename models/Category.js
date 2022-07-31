4e6d224067425f28229a3fb51667d921cdbb2b4a
panecatl <ypanecatl24@gmail.com>
2022-07-31 10:22:40 -0500

    fixing bugs

--- models/Category.js
+++ models/Category.js
@@ -15,11 +15,7 @@ Category.init(
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [1],
-      }
     },
-    
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,20 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Category.hasMany(Product, {
-  foreignKey: 'category_id'
-});
-
-// Categories have many Products
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE'
 });
 
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'ProductTag',
   foreignKey: 'product_id',
   onDelete: 'CASCADE'
 });
